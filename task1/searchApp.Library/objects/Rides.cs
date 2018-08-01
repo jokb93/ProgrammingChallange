877a51fd98404c8873cbbf0d627de647b15c8152
jokb93 <bertleren@gmail.com>
2018-08-01 10:15:12 +0200

    Initital setup
    Setup of tests and files

---
+++ task1/searchApp.Library/objects/Rides.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchApp.Library.objects
+{
+    public class Ride
+    {
+
+        String ID;
+        List<Connection> Connections;
+
+        public Ride(string ID, List<Connection> Connections)
+        {
+            this.ID = ID;
+            this.Connections = Connections;
+        }
+
+    }
+
+    public class Connection
+    {
+        string Start;
+        string Finish;
+        DateTime DepartureTime;
+        DateTime ArrivalTime;
+        string TrainName;
+        List<Fare> Fares;
+
+        public Connection(string Start, string Finish, DateTime DepartureTime, DateTime ArrivalTime, string TrainName, List<Fare> Fares)
+        {
+            this.Start = Start;
+            this.Finish = Finish;
+            this.DepartureTime = DepartureTime;
+            this.ArrivalTime = ArrivalTime;
+            this.TrainName = TrainName;
+            this.Fares = Fares;
+        }
+    }
+
+    public class Fare
+    {
+        string Name;
+        int Price;
+        string Currency;
+
+        public Fare(string Name, int Price, string Currency)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Currency = Currency;
+        }
+    }
+}
